a3764150f587ab70406924be9f5b43d01eadda84
Olivier <30660657+olivierstl@users.noreply.github.com>
2023-01-04 21:47:14 +0100

    feat: add winner & cell check

--- src/index.js
+++ src/index.js
@@ -30,6 +30,11 @@ class Board extends React.Component {
     /** Copy the array for modification */
     const squares = this.state.squares.slice()
 
+    /** If someone won or the square is already taken */
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
+
     squares[i] = this.state.isXNext ? 'X' : 'O'
 
     /** Update Board state */
@@ -54,7 +59,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player: ${this.state.isXNext ? 'X' : 'O'}`;
+    const winner = calculateWinner(this.state.squares)
+    let status
+    if (winner) {
+      status = `${winner} won`
+    } else {
+      status = `Next player: ${this.state.isXNext ? 'X' : 'O'}`
+    }
 
     return (
       <div>
@@ -95,6 +106,26 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
 }
 
 // ========================================
